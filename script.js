d30f6948934d08e14f91504da4ab64162d9c89bf
lauren <laurenagee@seattleacademy.org>
2024-01-29 13:53:41 -0800

    first

---
+++ script.js
@@ -0,0 +1,3 @@
+//we need to create code for the output because right now all we have is the input boxes which work but don't do anything when submitted
+//create a loop of divs
+//<div id="row" class="row"></div>
