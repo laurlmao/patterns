f40c46a1fde974ba8334becb43a3acd55da447c7
lauren <laurenagee@seattleacademy.org>
2024-02-02 12:16:54 -0800

    completed

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 //we need to create code for the output because right now all we have is the input boxes which work but don't do anything when submitted
 //create a loop of divs
 //<div id="row" class="row"></div>
+
+const rows = document.getElementById('rows');
+const columns = document.getElementById('columns');
+const color = document.getElementById('color');
+const submit = document.getElementById('submit');
+const grid = document.getElementById('grid');
+
+button.addEventListener("click", makeGrid);
+
+function makeGrid() {
+    grid.innerHTML = "";
+    for (let i = 0; i < rows.value; i++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row')
+        grid.appendChild(rowDiv);
+        for (let j=0; j < columns.value; j++) {
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.style.backgroundColor = color.value;
+        rowDiv.appendChild(box);
+        }
+    }
+}
+
+function changeColor(event) {
+
+}
